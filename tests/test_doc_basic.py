fc0e8e8b5e3ae7a89ff0514492b5e9defc902d63
Dima Unk <oil.crayons@gmail.com>
2023-09-13 06:47:08 +0200

    Tweak basic test to work

---
+++ tests/test_doc_basic.py
@@ -0,0 +1,26 @@
+from sharedb.doc import Doc, Op
+
+
+# noinspection PyUnresolvedReferences
+def test_doc_subscription_basic():
+    d = Doc.create({
+        'items': [
+            {'selected': True,
+             'bullets': ['one', 'two', 'three'],
+             'text': 'one something two something three something'},
+            {'selected': False,
+             'bullets': ['four', 'five', 'fix'],
+             'text': 'four something five something fix something'},
+            {'selected': True,
+             'bullets': ['eight', 'six', 'seven'],
+             'text': 'eight something six something seven something'},
+            {'selected': False,
+             'bullets': ['eighteen', 'twenty', 'eleven'],
+             'text': 'eighteen something twenty something eleven something'},
+        ]})
+
+    assert d['items', 0, 'bullets'] == ['one', 'two', 'three']
+    assert d['items', 1, 'bullets'] == ['four', 'five', 'fix']
+
+    d['items', 0, 'bullets'] = ['testing', 'bla']
+    assert d['items', 0, 'bullets'] == ['testing', 'bla']
