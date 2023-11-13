549579a2859b0ea542c798da3407575216feb196
Dima Unk <oil.crayons@gmail.com>
2023-11-13 18:33:39 +0100

    add simplistic setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="sharedb-ot-client",
+    version="1.0",
+    packages=find_packages(),
+    install_requires=[
+        'asyncio',
+        'quill-delta',
+        'websockets==11.*'
+    ]
+)
